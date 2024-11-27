b9e5218b1840add5a52df616e0ab1c5eda01c63f
Azeem Hasan Khan <khanazeemhasan@gmail.com>
2024-11-27 22:29:36 +0530

    project complted

--- server/conrollers/userController.js
+++ server/conrollers/userController.js
@@ -1,6 +1,7 @@
 import { Webhook } from "svix";
 import userModel from "../models/userModel.js";
-
+import razorpay from "razorpay";
+import transactionModel from "../models/transactionModel.js";
 //api controller function to manage clerk user with data base
 
 // http://localhost:4000/api/user/webhooks
@@ -57,4 +58,154 @@ const clerkWebHooks = async (req, res) => {
   }
 };
 
-export { clerkWebHooks };
+//api controller to get user credits
+
+const userCredits = async (req, res) => {
+  try {
+    const { clerkId } = req.body;
+
+    const userData = await userModel.findOne({ clerkId });
+    res.json({
+      success: true,
+      credits: userData.creditBalance,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+//gateway initiaize
+const razorpayInstance = new razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+//api to make payment
+const paymentRazorpay = async (req, res) => {
+  try {
+    const { clerkId, planId } = req.body;
+
+    const userData = await userModel.findOne({ clerkId });
+    // console.log(userData);
+    // console.log(planId)
+
+    if (!userData || !planId) {
+      res.json({
+        success: false,
+        message: "Invalid user or plan",
+      });
+    }
+    let credits, plan, amount, date;
+
+    switch (planId) {
+      case "Basic":
+        plan = "Basic";
+        credits = 100;
+        amount = 10;
+        break;
+
+      case "Advanced":
+        plan = "Advanced";
+        credits = 500;
+        amount = 50;
+        break;
+
+      case "Buisness":
+        plan = "Buisness";
+        credits = 5000;
+        amount = 250;
+        break;
+
+      default:
+        break;
+    }
+    date = Date.now();
+
+    const transactionData = {
+      clerkId,
+      plan,
+      amount,
+      credits,
+      date,
+    };
+
+    const newTransaction = await transactionModel.create(transactionData);
+
+    const options = {
+      amount: amount * 100,
+      currency: process.env.CURRENCY,
+      receipt: newTransaction._id,
+    };
+
+    await razorpayInstance.orders.create(options, (error, order) => {
+      if (error) {
+        console.log(error);
+        return res.json({
+          success: false,
+          message: error,
+        });
+      }
+      res.json({
+        success: true,
+        order,
+      });
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const verifyRazorpay = async (req, res) => {
+  try {
+    const { razorpay_order_id } = req.body;
+
+    const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id);
+
+    if (orderInfo.status === "paid") {
+      const transactionData = await transactionModel.findById(
+        orderInfo.receipt
+      );
+      console.log(transactionData);
+      if (transactionData.payment) {
+        return res.json({
+          success: false,
+          message: "Payment already processed",
+        });
+      }
+
+      const userData = await userModel.findOne({
+        clerkId: transactionData.clerkId,
+      });
+      const creditBalance = userData.creditBalance + transactionData.credits;
+      await userModel.findByIdAndUpdate(userData._id, { creditBalance });
+      await transactionModel.findByIdAndUpdate(transactionData._id, {
+        payment: true,
+      });
+      res.json({
+        success: true,
+        message: "Payment successful",
+      });
+    } else {
+      res.json({
+        success: false,
+        message: "Payment failed",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export { clerkWebHooks, paymentRazorpay, userCredits,verifyRazorpay };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { clerkWebHooks } from "../conrollers/userController.js";
+import { clerkWebHooks, paymentRazorpay, userCredits, verifyRazorpay } from "../conrollers/userController.js";
+import authUser from "../middlewares/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/webhooks", clerkWebHooks);
+userRouter.get("/credits", authUser, userCredits);
+userRouter.post("/pay-razor", authUser, paymentRazorpay);
+userRouter.post("/verify-razor", verifyRazorpay);
 
 export default userRouter;
